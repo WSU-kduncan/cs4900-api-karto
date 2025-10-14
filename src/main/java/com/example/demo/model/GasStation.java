a2b7f41f5ca3e6f51dda21eab9d41d26e7749a97
Owen <kemp.56@wright.edu>
2025-10-14 18:31:47 -0400

    Gas Station Model

---
+++ src/main/java/com/example/demo/model/GasStation.java
@@ -0,0 +1,40 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "gas_station")
+public class GasStation {
+    @Id
+    @Column(name = "station_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(name = "longitude", columnDefinition = "DECIMAL(9,6)", nullable = false)
+    Double longitude;
+
+    @Column(name = "latitude", columnDefinition = "DECIMAL(8,6)", nullable = false)
+    Double latitude;
+
+    @Column(name = "name", length = 25, nullable = false)
+    String name;
+
+    @Column(name = "address_line", length = 63, nullable = false)
+    String addressLine;
+
+    @Column(name = "city", length = 63, nullable = false)
+    String city;
+
+    @Column(name = "state", length = 15, nullable = false)
+    String state;
+
+    @Column(name = "zip_code", length = 10, nullable = false)
+    String zip;
+}
