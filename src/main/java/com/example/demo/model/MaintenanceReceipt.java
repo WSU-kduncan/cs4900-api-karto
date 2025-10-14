704a812c211b10d15d5ce4dd784b5d573aaaabc4
Owen <kemp.56@wright.edu>
2025-10-14 19:00:54 -0400

    Maintenance Receipt Model

---
+++ src/main/java/com/example/demo/model/MaintenanceReceipt.java
@@ -0,0 +1,14 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+
+public class MaintenanceReceipt {
+    @Id
+    @JoinColumn(name = "maintenance_id", nullable = false)
+    Maintenance maintenance;
+
+    @Column(name = "receipt_image", columnDefinition = "MEDIUMBLOB", nullable = false)
+    byte[] receipt;
+}
