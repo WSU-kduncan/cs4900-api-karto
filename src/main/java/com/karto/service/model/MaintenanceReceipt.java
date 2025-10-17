3133d09de98e52a0fb36066edd213d17359c614d
Porogaramu <112273919+Porogaramu@users.noreply.github.com>
2025-10-17 10:49:50 -0400

    Fix Maintenance Receipt (#8)

--- src/main/java/com/karto/service/model/MaintenanceReceipt.java
+++ src/main/java/com/karto/service/model/MaintenanceReceipt.java
@@ -1,12 +1,20 @@
 package com.karto.service.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Table(name = "maintenance_receipt")
+@Entity
+@Data
 public class MaintenanceReceipt {
     @Id
     @JoinColumn(name = "maintenance_id", nullable = false)
+    @OneToOne
     Maintenance maintenance;
 
     @Column(name = "receipt_image", columnDefinition = "MEDIUMBLOB", nullable = false)
