95e06c21472c119744048da99af8d113f0d492b1
Owen <kemp.56@wright.edu>
2025-10-19 20:55:54 -0400

    Updated Dtos and Models

--- src/main/java/com/karto/service/dto/MaintenanceItemDetailIdDto.java
+++ src/main/java/com/karto/service/dto/MaintenanceItemDetailIdDto.java
@@ -3,14 +3,10 @@ package com.karto.service.dto;
 import lombok.Builder;
 import lombok.Data;
 
-@Builder
 @Data
-public class MaintenanceItemDetailDto {
-    Integer quantity;
-
-    String comments;
-
-    MaintenanceItemDetailIdDto id;
+@Builder
+public class MaintenanceItemDetailIdDto {
+    Integer maintenanceId;
 
     MaintenanceTypeDescriptionDto maintenanceType;
 }
--- src/main/java/com/karto/service/model/Maintenance.java
+++ src/main/java/com/karto/service/model/Maintenance.java
@@ -7,9 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.List;
+import java.util.Set;
+
 import lombok.Data;
 
 @Entity
@@ -33,4 +37,7 @@ public class Maintenance {
 
   @Column(name = "cost", columnDefinition = "DECIMAL(7,4) UNSIGNED")
   BigDecimal cost;
+
+  @OneToMany(mappedBy = "id.maintenance")
+  Set<MaintenanceItemDetail> maintenanceItemDetails;
 }
