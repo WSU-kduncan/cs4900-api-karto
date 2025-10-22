e7a2ed22af84ba94aeb02eba2199f48a84149387
Owen <kemp.56@wright.edu>
2025-10-22 13:18:54 -0400

    Maintenance Receipt Update

--- src/main/java/com/example/demo/model/MaintenanceReceipt.java
+++ src/main/java/com/example/demo/model/MaintenanceReceipt.java
@@ -1,11 +1,23 @@
-package com.example.demo.model;
+package com.karto.service.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Table(name = "maintenance_receipt")
+@Entity
+@Data
 public class MaintenanceReceipt {
     @Id
+    Integer maintenanceId;
+
+    @OneToOne
+    @MapsId
     @JoinColumn(name = "maintenance_id", nullable = false)
     Maintenance maintenance;
 
