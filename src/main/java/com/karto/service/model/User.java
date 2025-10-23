d74f3588873af6c6f283be0157f959932bdc4144
JoshQuaintance <hasianjoshua@gmail.com>
2025-10-23 14:06:20 -0400

    Clean up

--- src/main/java/com/karto/service/model/User.java
+++ src/main/java/com/karto/service/model/User.java
@@ -1,11 +1,5 @@
 package com.karto.service.model;
 
-import java.time.Instant;
-import java.util.Set;
-
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.SourceType;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,32 +9,35 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.Set;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SourceType;
 
 @Data
 @Entity
 @Table(name = "user")
 public class User {
-    @Id
-    @Column(name = "user_email", length = 255, nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    String email;
-    
-    @Column(name = "username", length = 31, nullable = false)
-    String username;
+  @Id
+  @Column(name = "user_email", length = 255, nullable = false)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  String email;
 
-    @Column(name = "hashed_password", length = 127, nullable = false)
-    String password;
+  @Column(name = "username", length = 31, nullable = false)
+  String username;
 
-    @Column(name = "created_datetime")
-    @CreationTimestamp(source = SourceType.DB)
-    Instant createdAt;
+  @Column(name = "hashed_password", length = 127, nullable = false)
+  String password;
 
-    @JoinTable(
-        name = "trusted_gas_station",
-        joinColumns = @JoinColumn(name = "user_email"),
-        inverseJoinColumns = @JoinColumn(name = "station_id")
-    )
-    @ManyToMany
-    Set<GasStation> trustedGasStations;
+  @Column(name = "created_datetime")
+  @CreationTimestamp(source = SourceType.DB)
+  Instant createdAt;
+
+  @JoinTable(
+      name = "trusted_gas_station",
+      joinColumns = @JoinColumn(name = "user_email"),
+      inverseJoinColumns = @JoinColumn(name = "station_id"))
+  @ManyToMany
+  Set<GasStation> trustedGasStations;
 }
