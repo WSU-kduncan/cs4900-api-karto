260bb2bc4e0938c1a0cc1cb2d0be0af13364e78a
Blake Payne <56892670+payne1778@users.noreply.github.com>
2025-10-28 17:00:42 -0500

    Delete everything but TrustedGasStation & Gas Price items

--- src/main/java/com/karto/service/repository/MaintenanceReceiptRepository.java
+++
@@ -1,8 +0,0 @@
-package com.karto.service.repository;
-
-import com.karto.service.model.MaintenanceReceipt;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MaintenanceReceiptRepository extends JpaRepository<MaintenanceReceipt, Integer> {}
--- src/main/java/com/karto/service/repository/MaintenanceTypeDescriptionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.karto.service.repository;
-
-import com.karto.service.model.MaintenanceTypeDescription;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MaintenanceTypeDescriptionRepository
-    extends JpaRepository<MaintenanceTypeDescription, Integer> {}
