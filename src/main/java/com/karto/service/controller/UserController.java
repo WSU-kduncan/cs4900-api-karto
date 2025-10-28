99a91f0ac79f3cea2d78460509a04b0756e3c88d
Joshua Wise <joshua@Joshuas-MacBook-Air.local>
2025-10-27 23:04:38 -0400

    PUT and POST working for User and Gas Station

--- src/main/java/com/karto/service/controller/UserController.java
+++ src/main/java/com/karto/service/controller/UserController.java
@@ -5,19 +5,29 @@ import com.karto.service.mapper.UserDtoMapper;
 import com.karto.service.model.GasStation;
 import com.karto.service.model.User;
 import com.karto.service.service.UserService;
+
 import jakarta.persistence.EntityNotFoundException;
+
 import java.util.List;
+
+import org.apache.catalina.connector.Response;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
-@RequestMapping(path = "user", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "user", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class UserController {
 
@@ -63,4 +73,31 @@ public class UserController {
       return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
   }
+
+  @PostMapping()
+  public ResponseEntity<Object> createUser(@RequestBody UserDto userDto) {
+    try {
+      User user = userDtoMapper.toEntity(userDto);
+      User createdUser = userService.createUser(user);
+      return new ResponseEntity<>(userDtoMapper.toDto(createdUser), HttpStatus.CREATED);
+    } catch (IllegalArgumentException e) {
+      return new ResponseEntity<>(null, HttpStatus.CONFLICT);
+    } catch (Exception e) {
+      return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @PutMapping("{email}")
+  public ResponseEntity<Object> updateUser(@PathVariable String email, @RequestBody UserDto userDto) {
+    try {
+      User user = userDtoMapper.toEntity(userDto);
+      User updatedUser = userService.updateUser(email, user);
+      return new ResponseEntity<>(userDtoMapper.toDto(updatedUser), HttpStatus.OK);
+    } catch (EntityNotFoundException e) {
+      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+  }
+
 }
