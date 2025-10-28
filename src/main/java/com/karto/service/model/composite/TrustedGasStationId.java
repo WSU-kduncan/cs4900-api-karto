79e945addb905d23b4e75673f2017e8e64665a5e
Blake Payne <56892670+payne1778@users.noreply.github.com>
2025-10-28 16:26:30 -0500

    Update imports and make needed constructor

--- src/main/java/com/karto/service/model/composite/TrustedGasStationId.java
+++ src/main/java/com/karto/service/model/composite/TrustedGasStationId.java
@@ -2,10 +2,14 @@ package com.karto.service.model.composite;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TrustedGasStationId {
   @Column(name = "user_email", nullable = false)
   String userEmail;
