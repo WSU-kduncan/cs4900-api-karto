8175707d486ff916db95f81b60f142f5fefba50c
Owen <kemp.56@wright.edu>
2025-11-25 17:48:22 -0500

    Format

--- src/main/java/com/karto/service/repository/MaintenanceRepository.java
+++ src/main/java/com/karto/service/repository/MaintenanceRepository.java
@@ -1,7 +1,6 @@
 package com.karto.service.repository;
 
 import com.karto.service.model.Maintenance;
-
 import java.math.BigDecimal;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
